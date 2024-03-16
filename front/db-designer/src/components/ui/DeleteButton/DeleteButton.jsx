1ba23a8b5eaa8a41d8534907a3c12caf804de92d
F978566 <77454354+F978566@users.noreply.github.com>
2024-03-16 14:53:20 +0300

    change structure

--- front/db-designer/src/components/ui/DeleteButton/DeleteButton.jsx
+++
@@ -1,7 +0,0 @@
-const DeleteButton = ({ onClick }) => {
-    return (
-        <button onClick={onClick} className='btn btn-outline-danger'>Delete</button>
-    )
-}
-
-export default DeleteButton;
--- front/db-designer/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import project from './project';
-
-export default combineReducers({
-    auth,
-    project,
-});
